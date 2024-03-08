bc60dcd56ab9c5025f57338a52bd172af40090a5
arsalan-with-jawaid <159759857+arsalan-with-jawaid@users.noreply.github.com>
2024-03-08 08:16:57 +0000

    Add files via upload

---
+++ pm.js
@@ -0,0 +1,2 @@
+var user_name = 'Eric';
+console.log("hellow ".concat(user_name, " would you like to learn some python today?"));
---
+++ pm.ts
@@ -0,0 +1,2 @@
+let user_name: string='Eric'
+ console.log(`hellow ${user_name} would you like to learn some python today?`);
